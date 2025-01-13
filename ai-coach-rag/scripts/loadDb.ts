87d71699ac2700a12b4fa61819b01c37bd973855
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-13 00:17:58 -0600

    split data into chunks and converting into vectore form

--- ai-coach-rag/scripts/loadDb.ts
+++ ai-coach-rag/scripts/loadDb.ts
@@ -1,10 +1,13 @@
 import {DataAPIClient} from "@datastax/astra-db-ts";
 // import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web";
 // import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders";
 import OpenAI from "Openai";
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 
 import "dotenv/config"
+
+type SimiliarityMetric = "dot_product" | "cosine" | "euclidean"
 
 const {ASTRA_DB_NAMESPACE,ASTRA_DB_COLLECTION,ASTRA_DB_API_ENDPOINT,ASTRA_DB_APPLICATION_TOKEN,OPENAI_API_KEY} = process.env
 
@@ -23,3 +26,37 @@ const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 512,
     chunkOverlap: 100
 })
+
+const createCollection = async(similarityMetric: SimiliarityMetric = "dot_product") => {
+    const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+        vector:{
+            dimension: 1536,
+            metric: similarityMetric
+        }
+    })
+    console.log(res)
+}
+
+const loadSampleData = async() => {
+    const collection = db.collection(ASTRA_DB_COLLECTION)
+    for await(const url of aiCoachData){
+        const content = await scrapePage(url)
+        const chunks = await splitter.splitText(content)
+        for await (const chunk of chunks) {
+            const embedding = await openai.embeddings.create({
+                model: "text-embedding-3-small",
+                input: "utf",
+                encoding_format:"float"
+            })
+
+            const vector = embedding.data[0].embedding
+
+            const res = await collection.insertOne({
+                $vector: vector,
+                text: chunk
+            })
+            console.log(res);
+            
+        }
+    }
+}
